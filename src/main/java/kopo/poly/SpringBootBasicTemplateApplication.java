b9148b64f81ba89769c5696c90d0ddb02d8bc004
Hyeopgeon Lee <soft1318@naver.com>
2025-05-17 12:00:39 +0900

    소스 수정

--- src/main/java/kopo/poly/SpringBootBasicTemplateApplication.java
+++ src/main/java/kopo/poly/SpringBootBasicTemplateApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootBasicTemplateApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootBasicTemplateApplication.class, args);
     }
 
